6afb6b5ddb0317de933d4a05dc31c88930fc7478
jarvis <leguroky@gmail.com>
2017-02-21 17:49:21 +0800

    bundle all files to dist/react-weui.min.js

---
+++ webpack.library.js
@@ -0,0 +1,55 @@
+const path = require('path');
+const webpack = require('webpack')
+
+module.exports = {
+  devtool: 'sourcemap',
+
+  entry: {
+    'react-weui': './src/index.js'
+  },
+
+  output: {
+	path: path.resolve(__dirname, 'dist'),
+	filename: '[name].min.js',
+    library: 'react-weui',
+    libraryTarget: 'umd'
+  },
+
+  externals: [
+    {
+      react: {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react'
+      }
+    },
+    {
+      'react-dom': {
+        root: 'ReactDOM',
+        commonjs2: 'react-dom',
+        commonjs: 'react-dom',
+        amd: 'react-dom',
+      },
+    }
+  ],
+
+
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel' }
+    ]
+  },
+
+  node: {
+    Buffer: false
+  },
+
+  plugins: [
+    new webpack.optimize.OccurenceOrderPlugin(),
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    })
+  ]
+}
+
